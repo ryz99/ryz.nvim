a5285477fa0fb33e4db7c03aa20675eb45564381
ryz99 <rayaan.raza@tutanota.com>
2024-10-10 15:10:56 +0100

    modified java.lua to make things work

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,112 @@
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options
+
+-- Get the home directory
+local home = os.getenv 'HOME'
+
+-- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+local workspace_dir = home .. '/jdtls-workspace/' .. project_name
+
+local status, jdtls = pcall(require, 'jdtls')
+if not status then
+  return
+end
+
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+
 local config = {
-  cmd = { vim.fn.expand '~/.local/share/ryz-nvim/mason/bin/jdtls' },
-  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    'java', -- or '/path/to/java17_or_newer/bin/java'
+    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+
+    -- 💀
+    '-jar',
+    home .. '/.local/share/ryz-nvim/mason/share/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+    -- Must point to the                                                     Change this to
+    -- eclipse.jdt.ls installation                                           the actual version
+
+    -- 💀
+    '-configuration',
+    home .. '/.local/share/ryz-nvim/mason/packages/jdtls/config_linux',
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+    -- eclipse.jdt.ls installation            Depending on your system.
+
+    -- 💀
+    -- See `data directory configuration` section in the README
+    '-data',
+    workspace_dir,
+  },
+
+  -- 💀
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  --
+  -- vim.fs.root requires Neovim 0.10.
+  -- If you're using an earlier version, use: require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+  root_dir = vim.fs.root(0, { '.git', 'mvnw', 'gradlew' }),
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      extendedClientCapabilities = extendedClientCapabilities,
+      maven = {
+        downloadSources = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = 'all', -- literals, all, none
+        },
+      },
+      format = {
+        enabled = false,
+      },
+    },
+  },
+
+  -- For auto-completion (with method signatures and placeholders)
+  --capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  --flags = {
+  --allow_incremental_sync = true,
+  --},
+
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    bundles = {},
+  },
 }
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
 require('jdtls').start_or_attach(config)
